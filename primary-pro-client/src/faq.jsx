a65b01849c8f6b7b7dce5ba6303db0243beb33fe
brandonidun <88982788+brandonidun@users.noreply.github.com>
2023-07-21 06:44:14 +0000

    navbar & faq
    added nav menus to the navbar

--- primary-pro-client/src/faq.jsx
+++ primary-pro-client/src/faq.jsx
@@ -1,57 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Faq = () => {
-    return ( 
-        <div className="faq-banner">
-            <div className="faq-header">
-                <div className="header-writings">
-                    <h1>
-                     Plan Your Future
-                    </h1>
-                    <p>
-                    Get connected with everything you need to apply to college, research financial aid and scholarships, and get advice from counselors, advisors and mentors.
-                    </p>
-                </div>
-                <div className="header-svgs">
-
-                </div>
-            </div>
-            <div className="faq-cards">
-                <div className="why-school">
-                    <div className="why-school-svg"></div>
-                    <h1>WHY SCHOOL MATTERS</h1>
-                    <p>School is worth it. Your future is worth it. You are worth it.</p>
-                    <p>
-                        <Link to="/whyschool.jsx">
-                            <a href="">Learn why school</a>
-                        </Link>
-                    </p>
-                </div>
-                <div className="fees">
-                    <div className="fees-svg"></div>
-                    <h1>PAYING FOR SCHOOL</h1>
-                    <p>There are lots of options available to you. We can help you find them.</p>
-                    <p>
-                        <Link to='/fees.jsx'>
-                            <a href="">Learn how to pay for school</a>
-                        </Link>
-                    </p>
-                        
-                </div>
-                <div className="roadmap">
-                    <div className="roadmap-svg"></div>
-                    <h1>YOUR SCHOOL ROADMAP</h1>
-                    <p>The path may seem unclear. We can help you find your way.</p>
-                    <p>
-                        <Link  to="/roadmap.jsx">
-                            <a href="">Learn how to get to school</a>
-                        </Link>
-                        
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="faq-banner">
+      <div className="faq-header">
+        <div className="header-writings">
+          <h1>Plan Your Future</h1>
+          <p>
+            Get connected with everything you need to apply to college, research
+            financial aid and scholarships, and get advice from counselors,
+            advisors and mentors.
+          </p>
         </div>
-     );
-}
- 
+        <div className="header-svgs"></div>
+      </div>
+      <div className="faq-cards">
+        <div className="why-school">
+          <div className="why-school-svg"></div>
+          <h1>WHY SCHOOL MATTERS</h1>
+          <p>School is worth it. Your future is worth it. You are worth it.</p>
+          <p>
+            <Link to="/whyschool.jsx">
+              <a href="">{"Learn why school >"}</a>
+            </Link>
+          </p>
+        </div>
+        <div className="fees">
+          <div className="fees-svg"></div>
+          <h1>PAYING FOR SCHOOL</h1>
+          <p>
+            There are lots of options available to you. We can help you find
+            them.
+          </p>
+          <p>
+            <Link to="/fees.jsx">
+              <a href="">{"Learn how to pay for school >"}</a>
+            </Link>
+          </p>
+        </div>
+        <div className="roadmap">
+          <div className="roadmap-svg"></div>
+          <h1>YOUR SCHOOL ROADMAP</h1>
+          <p>The path may seem unclear. We can help you find your way.</p>
+          <p>
+            <Link to="/roadmap.jsx">
+              <a href="">{"Learn how to get to school >"}</a>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Faq;
--- primary-pro-client/src/navbar.jsx
+++ primary-pro-client/src/navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { CaretDownIcon } from "@radix-ui/react-icons";
 
 const Navbar = () => {
   return (
@@ -11,15 +12,46 @@ const Navbar = () => {
       </div>
       <div className="buttons">
         <ul>
-          <li>
-            <a href="">find a school</a>
-          </li>
-          <li>
-            <a href="">apply to a school</a>
-          </li>
-          <li>
-            <a href="">search</a>
-          </li>
+          <NavigationMenu.Root>
+            <NavigationMenu.List>
+              <NavigationMenu.Item>
+                <a href="">{"find a school"}</a>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item className="item">
+                <NavigationMenu.Trigger asChild>
+                  <a href="">Plan for college</a>
+                </NavigationMenu.Trigger>
+                <CaretDownIcon className="CaretDown" aria-hidden />
+                <NavigationMenu.Content className="item-content">
+                  <div className="nav-menu">
+                    <Link to="/whyschool.jsx">
+                      <li>Why school matters</li>
+                    </Link>
+                    <Link to="/fees.jsx">
+                      <li>Paying for school</li>
+                    </Link>
+                    <Link to="/roadmap.jsx">
+                      <li>your path to school</li>
+                    </Link>
+                  </div>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item className="item">
+                <NavigationMenu.Trigger asChild>
+                  <a href="">apply to a school</a>
+                </NavigationMenu.Trigger>
+                <CaretDownIcon className="CaretDown" aria-hidden />
+                <NavigationMenu.Content className="item-content">
+                  <Link to="/signup.jsx">
+                    <li>First time applicant</li>
+                  </Link>
+                  <Link to="/login.jsx">
+                    <li>Reapply</li>
+                  </Link>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Root>
           <Link to="/login.jsx">
             <li className="login-button">
               <a href="">login</a>
